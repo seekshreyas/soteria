d4c819ba05008167358bae82eb8ad1c5b3802cca
Shreyas <seekshreyas@gmail.com>
2014-04-28 17:52:49 -0700

    local changes before chicane meeting

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-var FEED = FEED || {};
+var SOTERIA = SOTERIA || {};
 
-FEED = (function($){
+SOTERIA = (function($){
 
 	var alldatalength = 0;
 
 	var init = function(){
 		console.log("page initiated");
-		var datapath = 'data/badcomments.json';
 
+		var datapath = 'data/vcdb_isotope.json';
 
-		// initDataLoad(datapath);
+		initDataLoad(datapath);
 
-		evtHandler();
+		// evtHandler();
 	};
 
 
@@ -30,140 +30,72 @@ FEED = (function($){
 
 	//event handler
 	var evtHandler = function(){
-		jQuery('input[type|=radio]').click(function(){
-			console.log(jQuery(this).val());
-
-			var btnval = jQuery(this).val();
-			var path = 'data/' + btnval + '.json';
-
-			initDataLoad(path);
-
-		});
+		console.log("event handler");
 	};
 
 	var renderFeed = function(alldata){
-		console.log("No of tweets: ", alldata.statuses.length);
+		console.log("No of hacks: ", alldata.length);
 
-		alldatalength = alldata.statuses.length
+		alldata.forEach(function(data, index){
 
-		jQuery('.feed').html('');
-		jQuery('.progress .bar').css({
-			'width' : '0'
-		});
+			if (index < 100){
+				console.log("Data: ", data, index);
+
+				// <div class="element-feed external" data-category="external">
+	   //              <p class="country">US</p>
+	   //              <p class="state">CA</p>
+	   //              <p class="victim_id">United States Department of Veterans Affairs</p>
+	   //              <p class="employee_count">5000</p>
+	   				// <p class="revenue_amount">5000</p>
+	   //              <p class="discovery_daycount">10</p>
+	   //              <p class="incident_year">2012</p>
+	   //            </div>
+
+				var attackInfo = {
+					attType		: data['actor.external'] == '1' ? 'external' : 'internal',
+					attVector	: getAttackVector(data),
+					attCountry 	: data['victim.country'],
+					attState 	: data['victim.state'],
+					attVictim 	: data['victim.victim_id'],
+					attEmpCount : data['victim.employee_count'],
+					attAmount 	: data['revenue.amount'],
+					attDayCount : data['timeline.discovery.day_count'],
+					attYear 	: data['timeline.incident.year']
+				};
 
 
-		alldata.statuses.forEach(function(data, index){
-			// check data
-			// console.log("interesting data points: ", data.entities, data.favorite_count);
+				var attackElem = '';
 
-			var feedInfo = {
-				index		: index,
-				hashtags 	: getHashTags(data.entities.hashtags),
-				author 		: data.user.name,
-				favcount 	: data.favorite_count,
-				retcount 	: data.retweet_count,
-				feedTxt 	: data.text
-				// feedSent 	: getFeedSentiment(data.text, index)
-			};
+				attackElem += '<div class="element-feed external" data-category="external">';
+				attackElem += '<p class="country">US</p>';
+				attackElem += '<p class="state">CA</p>';
+				attackElem += '<p class="victim_id">United States Department of Veterans Affairs</p>';
+				attackElem += '<p class="employee_count">5000</p>';
+				attackElem += '<p class="revenue_amount">5000</p>';
+				attackElem += '<p class="discovery_daycount">10</p>';
+				attackElem += '<p class="incident_year">2012</p>';
+				attackElem += '</div>';
+
+
+				jQuery('.feed').append(attackElem);
+			}
 			
-			console.log("Text: ", feedInfo.feedTxt, "feedHashTags: ", feedInfo.hashtags, "Names: ", feedInfo.author, "Favs: ", feedInfo.favcount, "Retweets: ", feedInfo.retcount);
 
-			//<div id="feed-1" class="element-feed neutral" data-category="neutral">
-          	//   <p class="tags">#tag1 #tag2</p>
-          	//   <p class="text">lorem ipsum dolor sit amet. A general neutral comment</p>
-          	//   <p class="author">@seekshreyas</p>
-          	//   <p class="sentiment">&#9205;</p>
-          	//   <p class="retweet">2</p>
-          	//   <p class="favorite">2</p>
-          	// </div>
-
-          	var feedElem = '';
-
-          	var size = (feedInfo.hashtags.length <= 2) ? 'width1' : 'width2';
-
-          	feedElem +=	'<div id="feed-' + index + '" class="element-feed neutral '+ size + '" data-category="neutral">';
-          	feedElem += '<p class="tags">' + feedInfo.hashtags.toString() + '</p>';
-          	feedElem += '<p class="text">' + feedInfo.feedTxt + '</p>';
-          	feedElem += '<p class="sentiment">&#9205;</p>';
-          	feedElem += '<p class="author">'+ feedInfo.author + '</p>';
-          	feedElem += '<p class="retweet">' + feedInfo.retcount + '</p>';
-          	feedElem += '<p class="favorite">' + feedInfo.favcount + '</p>';
-          	feedElem += '</div>';
-
-          	jQuery('.feed').append(feedElem);
 
 
 		});
+
+	
 	};
 
 
+	var getAttackVector = function(d){
 
-	var getFeedSentiment = function(txt, id){
+		// if d['']
 
-		// jQuery.ajax({
-		// 	url : 'sentiment',
-		// 	type : 'POST',
-		// 	data : {
-		// 		text : txt
-		// 	}
-		// })
-		// .done(function(sent){
-		// 	console.log("Sentiment: ", sent);
-
-		// 	elem = jQuery('#feed-' + id); //selector caching for faster lookup
-
-		// 	switch(sent[0]){
-
-		// 		case 'pos':
-		// 			//positive sentiment
-		// 			elem.children('.sentiment').html('&#58543;');
-		// 			elem.removeClass('neutral').addClass('positive');
-		// 			elem.attr('data-category', 'positive');
-
-		// 			elem.css({
-		// 				'background-color' : 'rgba(0, 240, 0,' + sent[1] + ')'
-		// 			});
-
-		// 			break;
-
-		// 		case 'neg':
-		// 			//positive sentiment
-		// 			elem.children('.sentiment').html('&#11015;');
-		// 			elem.removeClass('neutral').addClass('negative');
-		// 			elem.attr('data-category', 'negative');
-
-		// 			elem.css({
-		// 				'background-color' : 'rgba(240, 0, 0,' + sent[1] + ')'
-		// 			});
-
-		// 			break;
-
-		// 		default:
-		// 			console.log("no sentiment");
-
-		// 	}
-
-		// 	jQuery('.progress .bar').animate({
-		// 		'width' : String(Math.round((id + 1)/alldatalength * 100)) + '%'
-		// 	}, 200);
-			
-		// });
-
-		return 0.5;
+		return 0;
 	}
-
-
-	var getHashTags = function(hEntities){
-		var tags = [];
-
-		hEntities.forEach(function(t){
-			tags.push(t.text);
-		});
-
-		return tags; 
-	};
-
-
+	
 
 
 	// referred from: 
@@ -222,5 +154,5 @@ FEED = (function($){
 })(jQuery);
 
 jQuery(document).ready(function(){
-	FEED.init();
+	SOTERIA.init();
 });
